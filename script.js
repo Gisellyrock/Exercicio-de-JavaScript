fe618188b1ad0bddecaea49ea9bc866f3b060989
GisellyRock <gisellydsr@gmail.com>
2023-04-07 15:44:56 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,108 @@
+// 1 - Crie um alert no seu browser com a frase Hello World!.  - Introdução.
+
+// alert('Hello World!');
+
+// 2 - Imprima 'Hello World!' no console do browser.
+console.log('Hello World!');
+
+// 3 - Imprima uma variável que resulte undefined no console do browser. - Variável.
+var giselly;
+console.log(giselly);
+
+// 4- Inicialize uma variável com o valor 'Sou uma programadora'.
+
+var presente = 'Sou uma programadora';
+console.log(presente);
+
+// 5- Modifique a sua variável atribuindo o valor anterior por "Sou full stack".
+
+presente = 'Sou full stack';
+console.log(presente);
+
+// 6 - Atribua uma string a uma variável, depois reatribua com number. - Tipos de Dados.
+var bonitona = 'Sou eu!';
+bonitona = 21;
+console.log(bonitona);
+
+// 8 - Atribua a duas variáveis strings e concatene as duas.
+
+var number1 = '22';
+var number2 = '41';
+
+console.log(number1 + number2);
+
+// 9- Agora some os dois números atribuídos as variáveis.
+
+console.log(22 + 41);
+
+// 10- Atribua a uma variável o null.
+
+var safadao = null;
+console.log(safadao);
+
+// 11 - Atribua a uma variável um valor boolean. - Operador Lógico.
+var matematico = true;
+console.log(matematico);
+
+//7 - Atribua o retorno de uma variável com dus linhas diferentes. - Quebra de Linha
+
+var linda = 'sou mais eu \nnao quero voce!';
+console.log(linda);
+
+// 12 - Atribua dois números a duas variáveis e coloque o resultado numa terceira variável. - Operação Aritmética.
+
+var brad = 10;
+var tom = 20;
+
+var bradley = brad + tom;
+console.log(bradley);
+
+// 13- Crie outra variável  e diminua do total da anterior.
+var pobreza = 30 - bradley;
+
+console.log(pobreza);
+// 14 - Multiplique as variaveis.
+
+console.log(brad * bradley);
+
+// 15- Crie uma nova variável e divida.
+
+var vacilo = brad / tom;
+console.log(vacilo);
+
+// 16 - Tire o módulo das variáveis.
+
+var modulo = brad % tom;
+console.log(modulo);
+console.log(brad);
+console.log(tom);
+var modulo2 = tom % brad;
+console.log(modulo2);
+
+var a = 15;
+var b = 18;
+
+var modulo3 = a % b;
+console.log(modulo3);
+
+var modulo4 = b % a;
+console.log(modulo4);
+
+//17 - Faça o incremento de uma variavel.
+
+var alegria = 50;
+alegria = alegria + 1;
+
+console.log(alegria);
+
+alegria++;
+
+console.log(alegria);
+
+// 18 - Faça o incremento de uma variavel.
+
+var bobeira = 4;
+bobeira = bobeira - 1;
+console.log(bobeira);
+--bobeira;
+console.log(bobeira);
